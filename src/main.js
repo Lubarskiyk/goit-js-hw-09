db64e2a4fb30d2ce6122995737a68deae3e19529
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-06-03 09:21:38 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "../src/css/style.css";
-import javascriptLogo from "./javascript.svg";
+import javascriptLogo from "../javascript.svg";
 import { setupCounter } from "../counter.js";
 
 document.querySelector("#app").innerHTML = `
