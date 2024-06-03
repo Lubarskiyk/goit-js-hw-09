711911f817fd5d0b05773409c6f68f05e287f357
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-06-03 14:26:42 +0300

    23

---
+++ src/js/1-gallery.js
@@ -0,0 +1,16 @@
+import imageData from "../data/gallery.json";
+import SimpleLightbox from "simplelightbox";
+
+function markupImages(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery-item"> <a href="${original}" class="big gallery-link"><img src="${preview}" alt="" title="${description}" class="gallery-image"/></a> </li>`
+    )
+    .join("");
+}
+
+const galleryList = document.querySelector("#gallery");
+galleryList.insertAdjacentHTML("beforeend", markupImages(imageData));
+const gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {});
